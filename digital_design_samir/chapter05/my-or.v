38c8c5462e077358476f80ba9e0b0b0aca571f07
Patrick Dudjalija <patrick.dudjalija@gmail.com>
2018-02-13 20:08:09 +0100

    chap05 my-or

--- digital_design_samir/chapter05/my-or.v
+++ digital_design_samir/chapter05/my-or.v
@@ -1,11 +1,38 @@
-module my-or(a,b,out);
+module tb_my_or;
+	reg a,b;
+	wire out;
+
+	//instantiation
+	my_or my_or1(out,a,b);
+	
+	//start monitoring
+	initial
+		begin
+			$monitor($time," a=%b b=%b or=%b",a,b,out);
+		end
+		
+	initial
+		begin
+			   a=1'b0;b=1'b0;
+			#1 a=1'b0;b=1'b1;
+			#1 a=1'b1;b=1'b0;
+			#1 a=1'b1;b=1'b1;
+			#1 $finish;
+
+		end
+	
+endmodule
+
+
+
+module my_or(out,a,b);
 
 	//ports
 	input a,b;
 	output out;
 
 	//internal nets
-	wire e,f
+	wire e,f;
 
 	//instantiate primitives
 	nand(e,a,a);
